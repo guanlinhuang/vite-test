4d0b1349dd1a3d06efb05ec869718a02f5b9036a
Tony Huang <kuanlin830410@gmail.com>
2025-03-19 20:11:24 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: 'vite-test/',
+  base: '/vite-test/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
